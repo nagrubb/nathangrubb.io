42713364a2ba4da54ccef63be1efbc4660aaaf66
Nathan Grubb <nate@nathangrubb.io>
2021-12-27 16:07:25 -0800

    Update scripts

--- create_prod_containers.sh
+++ create_prod_containers.sh
@@ -2,5 +2,6 @@
 
 SCRIPT_DIR=$(dirname "$0")
 . ${SCRIPT_DIR}/vars.sh
+
 git -C ${SCRIPT_DIR} checkout ${PROD_BRANCH}
-PORT=${PROD_DOCKER_PORT} TAG=${PROD_DOCKER_TAG} /usr/local/bin/docker-compose -p ${PROD_DOMAIN_NAME} up -d --force-recreate
+PORT=${PROD_DOCKER_PORT} TAG=${PROD_DOCKER_TAG} docker-compose -f ${SCRIPT_DIR}/docker-compose.yml -p ${PROD_DOMAIN_NAME} up -d --force-recreate
--- create_staging_containers.sh
+++ create_staging_containers.sh
@@ -2,5 +2,6 @@
 
 SCRIPT_DIR=$(dirname "$0")
 . ${SCRIPT_DIR}/vars.sh
+
 git -C ${SCRIPT_DIR} checkout ${STAGING_BRANCH}
-PORT=${STAGING_DOCKER_PORT} TAG=${STAGING_DOCKER_TAG} /usr/local/bin/docker-compose -p ${STAGING_DOMAIN_NAME} up -d --force-recreate
+PORT=${STAGING_DOCKER_PORT} TAG=${STAGING_DOCKER_TAG} docker-compose -f ${SCRIPT_DIR}/docker-compose.yml -p ${STAGING_DOMAIN_NAME} up -d --force-recreate 
