d77e431839a5cb7c626073c3443fcccc6ffb452c
Nathan Grubb <nate@nathangrubb.io>
2021-12-27 16:23:55 -0800

    Remove staging and master branches and coverge to just one main branch

--- vars.sh
+++ vars.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-STAGING_BRANCH=staging
+STAGING_BRANCH=main
 STAGING_DOCKER_PORT=8000
 STAGING_DOCKER_TAG=staging
 STAGING_DOMAIN_NAME=staging.nathangrubb.io
 
-PROD_BRANCH=master
+PROD_BRANCH=main
 PROD_DOCKER_PORT=9000
 PROD_DOCKER_TAG=latest
 PROD_DOMAIN_NAME=nathangrubb.io
